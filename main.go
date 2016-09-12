22f6b7dec6290f34e7a51ff570b2a192837b137b
Ralph Caraveo III <deckarep@gmail.com>
2016-09-11 18:59:51 -0700

    Added  step, used web-server code, added port, added command to start the server, and finally used the docker-scratch to cut down on file size.

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello from Wercker")
+	fmt.Println("Server listening on :8080 ...")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
